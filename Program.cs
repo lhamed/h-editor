135f20c3acd27cd8624b1af854eb45b92d843b5e
Lhamed <lhamed.ludologist@gmail.com>
2026-06-24 20:02:20 +0900

    feat: 마스터데이터 페이지 추가 — Google Sheets CSV Fetch & Localization 빌드

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<HEditor.Components.App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(_ => new HttpClient());
 builder.Services.AddSingleton<DataStore>();
 builder.Services.AddScoped<ExportService>();
 
